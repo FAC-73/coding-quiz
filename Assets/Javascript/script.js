03d0a517d3d2401fc91b01bed347c0b7389ae2e4
fac-73 <kaydavis21@gmail.com>
2021-01-20 23:51:19 -0800

    Updating js, html and CSS, adding screenshots for README and copy

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -3,25 +3,25 @@
 
 // Questions, choices and answers       
 var questions = [{
-    question: "Which of the following will write the message “Hello DataFlair!” in an alert box?",
-    choices: ["alertBox(“Hello DataFlair!”);", "alert(Hello DataFlair!);", "msgAlert(“Hello DataFlair!”);", "alert(“Hello DataFlair!”);"],
-    correctAnswer: 3
+  question: "Which of the following will write the message “Hello DataFlair!” in an alert box?",
+  choices: ["alertBox(“Hello DataFlair!”);", "alert(Hello DataFlair!);", "msgAlert(“Hello DataFlair!”);", "alert(“Hello DataFlair!”);"],
+  correctAnswer: 3
 }, {
-    question: "How do you find the minimum of x and y using JavaScript?",
-    choices: ["min(x,y);", "Math.min(x,y)", "Math.min(xy)", "min(xy);"],
-    correctAnswer: 1
+  question: "How do you find the minimum of x and y using JavaScript?",
+  choices: ["min(x,y);", "Math.min(x,y)", "Math.min(xy)", "min(xy);"],
+  correctAnswer: 1
 }, {
-    question: "Which of the following statements will throw an error?",
-    choices: ["var fun = function bar( ){ }", "var fun = function bar{ }", "function fun( ){ }"],
-    correctAnswer: 1
+  question: "Which of the following statements will throw an error?",
+  choices: ["var fun = function bar( ){ }", "var fun = function bar{ }", "function fun( ){ }"],
+  correctAnswer: 1
 }, {
-    question: "Which JavaScript label catches all the values, except for the ones specified",
-    choices: ["catch", "label", "try", "default"],
-    correctAnswer: 3
+  question: "Which JavaScript label catches all the values, except for the ones specified",
+  choices: ["catch", "label", "try", "default"],
+  correctAnswer: 3
 }, {
-    question: "Which are the correct if statements to execute certain code if “x” is equal to 2?",
-    choices: ["if(x 2)", "if(x = 2)", "if(x == 2)", "if(x != 2 )"],
-    correctAnswer: 2
+  question: "Which are the correct if statements to execute certain code if “x” is equal to 2?",
+  choices: ["if(x 2)", "if(x = 2)", "if(x == 2)", "if(x != 2 )"],
+  correctAnswer: 2
 }];
 
 // Variables for storing numbers of correct questions
@@ -37,167 +37,164 @@ var startText = "Try to answer the following code related questions within the t
 
 // Fully loads HTML document before initializing controls
 $(document).ready(function () {
-        
-    // Display the introduction text and start button
-    $(".row").hide();
-    // $('.question').show();
-    $('.question').text(startText);
-    $("#nextButton").hide();
-    $("#saveButton").hide();
-    $(".result").hide();
-    $(this).find(".quizMessage").hide();
 
-    // Jquery eventListener triggers loading quiz content in via startQuiz function
-    $(this).find("#startButton").on("click", function () {
-        startQuiz()
-    });   
+  // Display the introduction text and start button
+  $(".row").hide();
+  // $('.question').show();
+  $('.question').text(startText);
+  $("#nextButton").hide();
+  $("#saveButton").hide();
+  $(".result").hide();
+  $("#saveScore").hide();
+  $(this).find(".quizMessage").hide();
 
-    // Jquery eventListener triggers dialog prompt asking for user initals + pre-populates with score
-    $(this).find("#saveButton").on("click", function () {
+  // Jquery eventListener triggers loading quiz content in via startQuiz function
+  $(this).find("#startButton").on("click", function () {
+    startQuiz()
+  });
 
-        var savedInitials = prompt("You got " + correctAnswers + " correct answers add your initials and click OK to save");
+  // Jquery eventListener displays save score input and displays number of correct answers
+  $(this).find("#saveButton").on("click", function () {
+   $("#saveScore").show();
+   $("#score").text("Your got " + correctAnswers + " / 5 answers correct");
+   $(".result").hide();
+   $(".question").text("Add your initials and click submit to save your score!");
+  });
 
-        // var response = initials.toLowerCase() === "Thanks for submitting your highscore":
-        // `${initials}`;
-        alert(response);
+  $(this).find("#submitButton").on("click", function(){ 
+    var initials = $(this).val();
+    $("#score").text(initials);
+  
+  });
 
-        if (savedInitials = null) {
-            return;
-        }
-        // else if {
-        //     localStorage.setItem(initialsText == savedInitials);
-        //     console.log(initialsText);
-        // }
-    }); 
+  // Event listener - iterates through the quiz questions
+  $(this).find("#nextButton").on("click", function () {
 
-        $(this).find("#nextButton").on("click", function () {
+    // Event listener - iterates through the quiz questions
+    if (!quizOver) {
 
-            if (!quizOver) {
+      value = $("input[type='radio']:checked").val();
 
-                value = $("input[type='radio']:checked").val();
-            
-                if (value == undefined) {
-                    $(document).find(".quizMessage").text("Please choose an answer from the options");
-                    $(document).find(".quizMessage").show();
+      // Checks to see if a radio input from the choices is selected, if not displays message to select an option
+      if (value == undefined) {
+        $(document).find(".quizMessage").text("Please choose an answer from the options");
+        $(document).find(".quizMessage").show();
 
-                } else {
-                    $(document).find(".quizMessage").hide();
-            
-                    if (value == questions[currentQuestion].correctAnswer) {
-                        correctAnswers++;
-                        // localStorage.setItem(correctAnswers);
-                    }
+      } else {
+        $(document).find(".quizMessage").hide();
 
-                    else if (value !== questions[currentQuestion].correctAnswer){
-                            displayIncorrectAnswer()
-                        }
-            
-                    currentQuestion++; // First question has already been displayed so increment to next
-                    if (currentQuestion < questions.length) {
-                        displayCurrentQuestion();
-                        displayScore();
-
-                    // quiz is over and total score is displayed - option to save score is shown
-                    } else {
-                        displayTotalScore();
-                        quizOver = true;
-                        $("#nextButton").text("Play again?");
-                        $("#saveButton").show();
-                        $(".choiceList").hide();
-                        $("#app").hide();
-                        onTimesUp();
-                        resetTimer();
-                    }
-                }
-
-            // reset the quiz and update nextButton text + hide save option
-            } else { 
-                quizOver = false;
-                resetQuiz();
-                displayCurrentQuestion();
-                $("#nextButton").text("Next question");
-                $("#saveButton").hide();
-            }
-        });
-    });
-    
-        // This displays the current question AND the choices
-        function displayCurrentQuestion() {
-        
-            console.log("In display current Question");
-        
-            var question = questions[currentQuestion].question;
-            var questionClass = $(document).find(".quizContainer > .question");
-            var choiceList = $(document).find(".column > .choiceList");
-            var numChoices = questions[currentQuestion].choices.length;
-        
-            // Set the questionClass text to the current question
-            $(questionClass).text(question);
-        
-            // Remove all current <li> elements (if any)
-            $(choiceList).find("li").remove();
-        
-            var choice;
-            for (i = 0; i < numChoices; i++) {
-                choice = questions[currentQuestion].choices[i];
-                $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
-            }
+        // Validates answer is correct from the choices, iterates to next question, displays correct answer message 
+        if (value == questions[currentQuestion].correctAnswer) {
+          correctAnswers++;
+          displayScore();
         }
 
-// Sets up UI ready for quiz, starts timer, loads questions and controls
-        function startQuiz() {
-            displayCurrentQuestion();
-            $("#counter").css("color", "black");
-            $("#nextButton").show();
-            $("#startButton").hide();
-            $(".row").show();
-            startTimer();
+        // Wrong answer triggers deduct 10 seconds function and displays incorrect answer message
+        else {
+          console.log("wrong answer")
+          displayIncorrectAnswer()
+          deductTime();
         }
+
+        // First question has already been displayed so increment to next
+        currentQuestion++; 
+        if (currentQuestion < questions.length) {
+          displayCurrentQuestion();
+
+        // quiz is over and total score is displayed - option to save score is shown
+        } else {
+          displayTotalScore();
+          quizOver = true;
+          $("#nextButton").text("Play again?");
+          $("#saveButton").show();
+          $(".choiceList").hide();
+          $("#app").hide();
+          onTimesUp();
+          resetTimer();
+        }
+      }
+
+      // reset the quiz and update nextButton text + hide save option
+    } else {
+      quizOver = false;
+      resetQuiz();
+      displayCurrentQuestion();
+      $("#nextButton").text("Next question");
+      $("#saveButton").hide();
+    }
+  });
+});
+
+// This displays the current question and choices
+function displayCurrentQuestion() {
+
+  console.log("In display current Question");
+
+  var question = questions[currentQuestion].question;
+  var questionClass = $(document).find(".quizContainer > .question");
+  var choiceList = $(document).find(".column > .choiceList");
+  var numChoices = questions[currentQuestion].choices.length;
+
+  // Set the questionClass text to the current question
+  $(questionClass).text(question);
+
+  // Remove all current <li> elements (if any)
+  $(choiceList).find("li").remove();
+
+  var choice;
+  for (i = 0; i < numChoices; i++) {
+    choice = questions[currentQuestion].choices[i];
+    $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+  }
+}
+
+// Sets up questions ready for quiz, starts timer, loads questions and buttons
+function startQuiz() {
+  displayCurrentQuestion();
+  $("#nextButton").show();
+  $("#startButton").hide();
+  $(".row").show();
+  startTimer();
+}
 
 // Resets the question and answer variables to play quiz again     
-        function resetQuiz() {
-            currentQuestion = 0;
-            correctAnswers = 0;
-            hideScore();
-            $(".choiceList").show();
-            $("#app").show();
-            startTimer();
-        }
-        
+function resetQuiz() {
+  currentQuestion = 0;
+  correctAnswers = 0;
+  hideScore();
+  $(".choiceList").show();
+  $("#app").show();
+  startTimer();
+}
+
 // Displays score when a question is answered correctly
-        function displayScore() {
-            $(document).find(".column > .result").text("Correct answer!");
-            $(".result").css({"color": "#155724", "border": "1px solid #C3E6CB", "background-color": "#D4EDDA"});
-            $(".result").addClass(".result-success");
-            // $(document).find(".quizContainer > .result").show();
-            $(".result").fadeIn("slow").show();
-        }
+function displayScore() {
+  $(document).find(".column > .result").text("Correct answer!");
+  $(".result").css({ "color": "#155724", "border": "1px solid #C3E6CB", "background-color": "#D4EDDA" });
+  $(".result").addClass(".result-success");
+  $(".result").fadeIn("slow").show();
+}
 
 // Displays message that answer is incorrect - updates timer to remove 10s from current time elapsed  - NEED HELP HERE
-        function displayIncorrectAnswer() {
-            $(document).find(".column > .result").text("Incorrect answer, 10 seconds have been deducted from your quiz time");
-            // $(document).find(".quizContainer > .result").show();
-            $(".result").fadeIn("slow").show();
-            $(".result").css({"color": "#721C24", "border": "1px solid #F5C6CB", "background-color": "#F8D7DA"});
-            $(document).find("#counter").setInterval()(minutes, seconds, - 10000);
-            currentQuestion++;
-        }
+function displayIncorrectAnswer() {
+  console.log("display incorrect answer")
+  $(document).find(".column > .result").text("Incorrect answer, 10 seconds have been deducted from your quiz time");
+  $(".result").fadeIn("slow").show();
+  $(".result").css({ "color": "#721C24", "border": "1px solid #F5C6CB", "background-color": "#F8D7DA" });
+}
 
 // Displays total answers at end of the quiz and prompts if the user wants to save their score
-        function displayTotalScore() {
-            $(document).find(".column > .result").text("Your total score is: " + correctAnswers + " out of: " + questions.length);
-            // $(document).find(".quizContainer > .result").show();
-            $(".result").fadeIn("slow").show();
-            $(".result").css("color", "black");
-            $('.question').text("Would you like to save your score?");
-        }
+function displayTotalScore() {
+  $(document).find(".column > .result").text("Your total score is: " + correctAnswers + " out of: " + questions.length);
+  $(".result").fadeIn("slow").show();
+  $(".result").css({ "color": "black", "border": "1px solid #d1d1d1", "background-color": "#f0f0f0" });
+  $('.question').text("Would you like to save your score?");
+}
 
 // Hides the result html div
-        function hideScore() {
-            $(document).find(".result").hide();
-        }
-
-
+function hideScore() {
+  $(document).find(".result").hide();
+}
 
 // Timer function
 
@@ -211,22 +208,30 @@ let timerInterval = null;
 document.getElementById("app").innerHTML = `
 <div class="base-timer">
   <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-  )}</span>
+  timeLeft
+)}</span>
 </div>
 `;
 
-// Stops the timer once timelimt is reached
+// Stops the timer once timelimit is reached skips
 function onTimesUp() {
   clearInterval(timerInterval);
-  // alert("You've run out of time");
-  // $(".row").hide()
-  // $(".question").text("Click the play again button to restart the quiz")
+  timePassed = 0;
+  document.getElementById("base-timer-label").innerHTML = formatTime(timePassed);
+  displayTotalScore();
+  quizOver = true;
+  $("#nextButton").text("Play again?");
+  $("#saveButton").show();
+  $(".choiceList").hide();
+  $("#app").hide();
+  $(document).find(".quizMessage").hide();
 }
 
 // Clears and resets the timer to original limit in time limit variable
 function resetTimer() {
-  clearInterval(timeLeft, timePassed);
+  clearInterval(timerInterval);
+  timePassed = 0;
+  timeLeft = TIME_LIMIT;
   document.getElementById("base-timer-label").innerHTML = formatTime(TIME_LIMIT);
 }
 
@@ -239,7 +244,22 @@ function startTimer() {
       timeLeft
     );
 
-    if (timeLeft === 0) {
+    if (timeLeft <= 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+// Restarts the timer after it has had time deducted
+function restartTimer(time) {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = time - timePassed;
+    document.getElementById("base-timer-label").innerHTML = formatTime(
+      timeLeft
+    );
+
+    if (timeLeft <= 0) {
       onTimesUp();
     }
   }, 1000);
@@ -255,5 +275,14 @@ function formatTime(time) {
   }
 
   return `${minutes}:${seconds}`;
+}
+
+// Deducts the time from the running timer for incorrect answers
+function deductTime() {
+  console.log(timeLeft);
+  clearInterval(timerInterval);
+  timeLeft -= 10;
+  document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
+  restartTimer(timeLeft);
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,181 @@
 # coding-quiz
-A timed coding quiz application with multiple-choice questions. This app runs in the browser and includes dynamically updated HTML and CSS powered by JavaScript. It also includes a fully responsive interface that can scale from large screens to small
+A timed coding quiz application with multiple-choice questions. This app runs in the browser and includes dynamically updated HTML and CSS powered by JavaScript and Jquery. It also includes a fully responsive interface that can scale from large screens to small
+
+
+![Website screenshot](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/copy3.png?raw=true "Password Generator App")
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+     <li>
+      <a href="https://fac-73.github.io/coding-quiz/">View project</a></li>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+![Password Generator Screenshot](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/Start.png?raw=true "Password Generator Screenshot")
+<br><br>
+
+**Built using Javascript, Jquery, HTML and CSS. This timed Javascript coding quiz is an application that provides a set of mulitple choice questions on Javascript programming. Each incorrect answer deducts 10 seconds from the timer. If time runs out the quiz is reset and the user is able to restart the quiz again. If the user manages to answer all questions in the allocated time they can save their score along with their initials.**
+
+
+### Built With
+
+* [HTML](https://www.w3schools.com/)
+* [CSS](https://www.w3schools.com/)
+* [Javascript](https://www.w3schools.com/))
+* [Jquery](https://jquery.com/))
+* [FontAwesome](https://fontawesome.com/)
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps. You can also download the source files provided. You will need a text editor such as Visual Studio Code, Xcode or similar to edit the source code.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/FAC-73/coding-quiz.git
+   ```
+
+2. Pull the latest
+   ```sh
+   git pull
+   ```
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+![Password generator](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/Start.png?raw=true "Password Generator")
+
+#### What's included
+1. HTML, CSS and Javascript (incl. Jquery) files include the source code for running the timed quiz application
+2. This project demonstrates ways in which javavscript and jquery can be used to manipulate the DOM elements, store data in variables, and dynamically update HTML and trigger interactions through the use of Jquery eventListeners.
+
+
+## Project deliverables
+
+#### Included functionality
+The start quiz button includes an eventListener that initializes the quiz questions and starts a dynamic timer that starts at 01m:00s.
+
+1. For each of the five questions provided select an answer using the inputs, then click next question.
+
+![Password Generator prompt](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/promptValue.png?raw=true "Password Generator prompt")
+<br>
+
+![Password Generator prompt invalid](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/invalidLength.png?raw=true "Password Generator prompt invalid")
+<br>
+
+2. If the answer for each question is incorrect you'll be notifed, along with a 10s deduction from the running timer and moved on to the next question
+
+![Cancel action](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/cancelNull.png?raw=true "Cancel action")
+<br>
+
+3. Correct answers are notified in green and no time is deducted. Correct answers are stored in variable and incremented as you move through the 5 questions
+
+![Password Generator confirm password parameters](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/IncludeParamenters.png?raw=true "confirm password parameters")
+<br>
+
+4. If the user does not select a checkbox from the list of questions the value is marked as undefined and the user is prompted to select a question before they are able to move onto the next question
+
+![Include at least one parameter](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/invalidCriteria.png?raw=true "Include at least one parameter")
+<br>
+
+5. If the user runs out of time before reaching the last question the quiz is automatically reset (questions, total scores and timer) to the beginning. 
+
+6. If the user manages to answer all questions in the alloted time they are shown their total score and provided with two options: 
+- Play again (This restarts the quiz from the beginning)
+- Save score (this displays a form where they can submit their initials)
+
+7. The save score form displays a text input where the user can type their initials, along with a submit button to save it. Their score number is also displayed above the input
+
+![Generate password](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/generatePassword.png?raw=true "generate password")
+<br>
+
+8. Their saved score and initials is displayed into a list on submit. Along with two more options:
+- Remove score
+- Play again
+
+Remove score deletes the list item. Play again restarts the quiz from the beginning.
+
+![Copy password](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/copy.png?raw=true "copy password")
+<br>
+
+![Copy password confirm](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/copy3.png?raw=true "copy password confirm")
+<br>
+
+
+#### HTML, CSS and Javascript
+1. Index.html - Contains basic layout structure using a custom div element named 'quizContainer' to display the main content for the quiz such as question text, questions list, timer and form elements. 
+Semantic html is used for elements such as buttons, inputs, footers, header, h1, li. 
+
+2. styles.css - Contains layout, styling and media-queries for html content
+
+3. script.js - Contains Variables, variable arrays and objects for the questions and multiple choice answers, variable for correct answer types. 
+
+Four eventListeners one to iterate through the questions, one to start the quiz, one to reset it and another to submit form details. 
+
+A loop for iterating through the questions list and generating the new choice list for each one. 
+
+A setInterval timer function that's triggered by an eventListener, functions to reset, deduct and restart the timer. 
+
+If, Else statements for validating whether the selected answer is correct, or undefined. If correct they are iterated onto the next question and the correct score is added to the variable correctAnswers, along with a correct message indicator. 
+
+If incorrect 10s is removed from the running timer using the deductTime function, an incorrect message is displayed. If an incorrect answer takes the timer to a value of <=0 the quiz is automatically restarted
+
+
+
+#### Responsive view example - On mobile
+![Responsive layout](https://github.com/FAC-73/password-generator/blob/main/Assets/Images/responsive.PNG?raw=true "Responsive views")
+
+
+#### Pushing to GitHub
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/FeatureName`)
+3. Commit your Changes (`git commit -m 'Add some FeatureName`)
+4. Push to the Branch (`git push origin feature/FeatureName`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. 
+
+See [LICENSE](https://github.com/FAC-73/coding-quiz/blob/main/LICENSE) for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Kay Davis
+
+Project repo link: [https://fac-73.github.io/coding-quiz/](https://fac-73.github.io/coding-quiz/
+<br>
+Project website: [https://github.com/FAC-73/coding-quiz](https://github.com/FAC-73/coding-quiz)
